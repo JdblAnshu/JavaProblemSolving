c30dc4d90ab8bfc725085db24d6f583e7893bf16
anshukumar333 <anshukumar.c@thoughtworks.com>
2025-01-06 21:24:13 +0530

    Day 5: Linked list loop and Basic operation.

--- src/LinkedListSeries/LinkedListBasicOperation.java
+++ src/LinkedListSeries/LinkedListBasicOperation.java
@@ -1,2 +1,30 @@
 public class LinkedListBasicOperation {
+
+	public static void insertAtStart(){
+
+	}
+
+
+	public static void printLinkedList(){
+
+	}
+
+
+	public static void insertAfterGivenNodeLinkedList(){
+
+	}
+
+
+	public static void insertBeforeGivenNodeLinkedList(){
+
+	}
+
+
+
+	public static void main(String[] args) {
+
+	}
+
+
+
 }
--- src/LinkedListSeries/RemoveNthElementFromEndInLinkedList.java
+++ src/LinkedListSeries/RemoveNthElementFromEndInLinkedList.java
@@ -1,2 +1,34 @@
 public class RemoveNthElementFromEndInLinkedList {
+
+
+	public static void removeNthNodeFromEnd(Node head){
+
+		int initialCnt = 1;
+		Node temp = head;
+		while (temp!=null && initialCnt != 4){
+			System.out.print(temp.data + " ");
+			temp = temp.next;
+			initialCnt++;
+		}
+
+	}
+
+
+	public static void main(String[] args) {
+		Node head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		head.next.next.next = new Node(4);
+		head.next.next.next.next = new Node(5);
+		head.next.next.next.next.next = new Node(7);
+
+		Node temp = head;
+		//Node reversedHead = reverseLinkedList(head);
+		removeNthNodeFromEnd(head);
+
+//		while (temp!=null){
+//			System.out.print(temp.data + " ");
+//			temp = temp.next;
+//		}
+	}
 }
