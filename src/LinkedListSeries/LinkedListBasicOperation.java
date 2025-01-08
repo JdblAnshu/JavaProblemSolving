d2e11f7ceb4bf3581446f12d3ce5246e81638d9d
anshukumar333 <anshukumar.c@thoughtworks.com>
2025-01-08 23:17:18 +0530

    Day 6: Linked list Basic operation and Balanced parenthesis

--- src/LinkedListSeries/LinkedListBasicOperation.java
+++ src/LinkedListSeries/LinkedListBasicOperation.java
@@ -1,7 +1,13 @@
 public class LinkedListBasicOperation {
 
-	public static void insertAtStart(){
+	public static Node insertAtStart(Node head){
+		Node temp = head;
 
+		Node newNode = new Node(45);
+		
+
+
+	return head;
 	}
 
 
@@ -22,6 +28,21 @@ public class LinkedListBasicOperation {
 
 
 	public static void main(String[] args) {
+
+		Node head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		head.next.next.next = new Node(4);
+		head.next.next.next.next = new Node(5);
+		head.next.next.next.next.next = new Node(7);
+
+		Node temp = insertAtStart(head);
+
+
+		while (temp!=null){
+			System.out.print(temp.data + " ");
+			temp = temp.next;
+		}
 
 	}
 
--- src/StackSeries/CheckBalancedParenthesis.java
+++ src/StackSeries/CheckBalancedParenthesis.java
@@ -1,3 +1,38 @@
+import java.util.Stack;
+
 public class CheckBalancedParenthesis {
+
+
+	public static boolean checkBalancedParenthesis(String str){
+
+		Stack<Character> stk = new Stack<>();
+		for(int i = 0; i< str.length(); i++){
+			if(str.charAt(i) == '(' || str.charAt(i) == '{' || str.charAt(i) == '[' ){
+				stk.push(str.charAt(i));
+			}else{
+				if(!stk.isEmpty() && (
+						(stk.peek() == '{' && str.charAt(i) == '}' ) ||
+								(stk.peek() == '(' && str.charAt(i) == ')' )||
+								(stk.peek() == '[' && str.charAt(i) == ']'))){
+					stk.pop();
+				}else {
+					return false;
+				}
+			}
+		}
+
+
+		return stk.isEmpty();
+	}
+
+	public static void main(String[] args) {
+		String s = "{()}[]";
+		if (checkBalancedParenthesis(s))
+			System.out.println("true");
+		else
+			System.out.println("false");
+
+
+	}
 
 }
