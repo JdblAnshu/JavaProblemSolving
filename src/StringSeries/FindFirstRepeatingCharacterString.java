43656b583807f3065498574541e8c0304869d07a
anshukumar333 <anshukumar.c@thoughtworks.com>
2025-03-16 15:35:22 +0530

    Day 16: String questions.

--- src/StringSeries/FindFirstRepeatingCharacterString.java
+++ src/StringSeries/FindFirstRepeatingCharacterString.java
@@ -1,3 +1,32 @@
-public class FindFirstRepeatingCharacterString {
+import java.util.HashSet;
+import java.util.Set;
 
+public class FindFirstRepeatingCharacterString {
+	public static void main(String[] args) {
+		String str = "abbca";
+//
+//	Set<Character> set= new HashSet<>();
+//
+//	for(int i=0;i < str.length();i++){
+//		if(set.contains(str.charAt(i))){
+//			System.out.println("First repeating character " + str.charAt(i));
+//			break;
+//		}else{
+//			set.add(str.charAt(i));
+//		}
+//	}
+
+	////////////////////////////////
+	//Frequnecy of element
+	int[] charCount = new int[26];
+	for(int i=0; i<str.length(); i++){
+		charCount[str.charAt(i) - 'a']++;
+	}
+
+	for(Integer c: charCount){
+		System.out.println(c);
+	}
+
+
+	}
 }
--- src/StringSeries/FirstNonRepeatingCharacterString.java
+++ src/StringSeries/FirstNonRepeatingCharacterString.java
@@ -1,8 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class FirstNonRepeatingCharacterString {
 
 	public static void main(String[] args) {
 
 		String input = "swiss";
+		Map<Character,Integer> map= new HashMap<>();
+
+		for(int i = 0; i<input.length();i++){
+			if(map.containsKey(input.charAt(i))){
+				map.put(input.charAt(i), map.getOrDefault(input.charAt(i),0)+1);
+			}else {
+
+			}
+		}
+//Solution
+//		HashMap<Character, Integer> charCountMap = new HashMap<>();
+//
+//		// First, iterate over the string to count the frequency of each character
+//		for (char c : input.toCharArray()) {
+//			charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+//		}
+//
+//		// Now, iterate through the string again to find the first character with a frequency of 1
+//		for (char c : str.toCharArray()) {
+//			if (charCountMap.get(c) == 1) {
+//				return c;  // Return the first non-repeating character
+//			}
+//		}
 
 
 
